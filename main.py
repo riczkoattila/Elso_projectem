3fb6cefdc9ec33ba0ff46463785c09fc25b44ae2
riczkoattila <riczkoattila@gmail.com>
2025-09-13 10:46:29 +0200

    Elso modositas

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm, Riczkó Attila: WQPEQO')
+    print_hi('PyCharm, Riczkó Attila: WQPEQO ')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
